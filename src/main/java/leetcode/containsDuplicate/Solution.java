99287c6d31774414c67d69d2361475f6c5d9f231
Mykola Pavluchynskyi <mpa@encode.dk>
2023-10-20 19:57:14 +0200

    217. Contains Duplicate - evolution of solutions

--- src/main/java/leetcode/containsDuplicate/Solution.java
+++ src/main/java/leetcode/containsDuplicate/Solution.java
@@ -2,6 +2,7 @@ package leetcode.containsDuplicate;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -36,6 +37,31 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @SuppressWarnings("java:S5960")
 public class Solution {
 
+    // Time: O(n^2)
+    // Space: O(1)
+    public boolean containsDuplicateBrutforce(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                if (nums[j] == nums[i]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // Time: O(n log n)
+    // Space: O(1)
+    public boolean containsDuplicateSorted(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length-1; i++) {
+            if (nums[i+1] == nums[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     // Time: O(n)
     // Space: O(n)
     public boolean containsDuplicate(int[] nums) {
@@ -53,6 +79,8 @@ public class Solution {
     public void testContainsDuplicateTrue() {
         int[] nums = {1,2,3,1};
 
+        assertTrue(new Solution().containsDuplicateBrutforce(nums));
+        assertTrue(new Solution().containsDuplicateSorted(nums));
         assertTrue(new Solution().containsDuplicate(nums));
     }
 
@@ -60,6 +88,8 @@ public class Solution {
     public void testContainsDuplicateFalse() {
         int[] nums2 = {1,2,3,4};
 
+        assertFalse(new Solution().containsDuplicateBrutforce(nums2));
+        assertFalse(new Solution().containsDuplicateSorted(nums2));
         assertFalse(new Solution().containsDuplicate(nums2));
     }
 
