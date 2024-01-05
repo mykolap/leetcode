db1d413a503ff2dc76bfd4b5b84fdb37d35a18d4
Mykola Pavluchynskyi <mpa@encode.dk>
2024-01-05 23:00:24 +0100

    102. Binary Tree Level Order Traversal

---
+++ src/main/java/leetcode/binaryTreeLevelOrderTraversal/Solution0102.java
@@ -0,0 +1,68 @@
+package leetcode.binaryTreeLevelOrderTraversal;
+
+import leetcode.common.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * <a href="https://leetcode.com/problems/binary-tree-level-order-traversal/">
+ * 102. Binary Tree Level Order Traversal
+ * </a>
+ * <p>
+ * Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right,
+ * level by level).
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: root = [3,9,20,null,null,15,7]
+ * Output: [[3],[9,20],[15,7]]
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: root = [1]
+ * Output: [[1]]
+ * <p>
+ * Example 3:
+ * <p>
+ * Input: root = []
+ * Output: []
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * The number of nodes in the tree is in the range [0, 2000].
+ * -1000 <= Node.val <= 1000
+ * <p>
+ * <p>
+ * Follow-up: Recursive solution is trivial, could you do it iteratively?
+ */
+public class Solution0102 {
+
+    // Time O(n)
+    // Space O(n)
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return List.of();
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>(size);
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+            }
+            result.add(level);
+        }
+
+        return result;
+    }
+}
---
+++ src/test/java/leetcode/binaryTreeLevelOrderTraversal/Solution0102Test.java
@@ -0,0 +1,39 @@
+package leetcode.binaryTreeLevelOrderTraversal;
+
+import leetcode.common.TreeNode;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class Solution0102Test {
+
+    static Stream<Arguments> casesProvider() {
+        return Stream.of(
+                Arguments.of(
+                        TreeNode.fromVarargArray(3, 9, 20, null, null, 15, 7),
+                        List.of(List.of(3), List.of(9, 20), List.of(15, 7))
+                ),
+                Arguments.of(
+                        TreeNode.fromVarargArray(1),
+                        List.of(List.of(1))
+                ),
+                Arguments.of(
+                        TreeNode.fromVarargArray(),
+                        List.of()
+                )
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("casesProvider")
+    void levelOrder(TreeNode head, List<List<Integer>> expected) {
+        assertThat(new Solution0102().levelOrder(head))
+                .containsExactlyElementsOf(expected);
+    }
+
+}
