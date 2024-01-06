76a5168c3f577773fa905c7c8c07e79c0b9dd25b
Mykola Pavluchynskyi <mpa@encode.dk>
2024-01-06 18:58:24 +0100

    10033. Minimum Number of Operations to Make X and Y Equal

---
+++ src/main/java/leetcode/minimumNumberOfOperationsToMakeXAndYEqual/Solution10033.java
@@ -0,0 +1,119 @@
+package leetcode.minimumNumberOfOperationsToMakeXAndYEqual;
+
+import java.util.*;
+
+/*
+* https://leetcode.com/problems/minimum-number-of-operations-to-make-x-and-y-equal/description/
+* 10033. Minimum Number of Operations to Make X and Y Equal
+* Medium
+* You are given two positive integers x and y.
+
+In one operation, you can do one of the four following operations:
+
+Divide x by 11 if x is a multiple of 11.
+Divide x by 5 if x is a multiple of 5.
+Decrement x by 1.
+Increment x by 1.
+Return the minimum number of operations required to make x and y equal.
+
+Example 1:
+
+Input: x = 26, y = 1
+Output: 3
+Explanation: We can make 26 equal to 1 by applying the following operations:
+1. Decrement x by 1
+2. Divide x by 5
+3. Divide x by 5
+It can be shown that 3 is the minimum number of operations required to make 26 equal to 1.
+Example 2:
+
+Input: x = 54, y = 2
+Output: 4
+Explanation: We can make 54 equal to 2 by applying the following operations:
+1. Increment x by 1
+2. Divide x by 11
+3. Divide x by 5
+4. Increment x by 1
+It can be shown that 4 is the minimum number of operations required to make 54 equal to 2.
+Example 3:
+
+Input: x = 25, y = 30
+Output: 5
+Explanation: We can make 25 equal to 30 by applying the following operations:
+1. Increment x by 1
+2. Increment x by 1
+3. Increment x by 1
+4. Increment x by 1
+5. Increment x by 1
+It can be shown that 5 is the minimum number of operations required to make 25 equal to 30.
+
+
+Constraints:
+
+1 <= x, y <= 104
+ */
+public class Solution10033 {
+
+    // Time: O(n)
+    // Space: O(n)
+    public int minimumOperationsToMakeEqual(int x, int y) {
+        if (x == y) {
+            return 0;
+        }
+        if (x < y) {
+            return y - x;
+        }
+
+        int maxNumberOfSteps = x - y;
+
+        maxNumberOfSteps = Math.min(maxNumberOfSteps, 1 + x % 11 + minimumOperationsToMakeEqual(x / 11, y));
+        maxNumberOfSteps = Math.min(maxNumberOfSteps, 1 + 11 - x % 11 + minimumOperationsToMakeEqual(x / 11 + 1, y));
+
+        maxNumberOfSteps = Math.min(maxNumberOfSteps, 1 + x % 5 + minimumOperationsToMakeEqual(x / 5, y));
+        maxNumberOfSteps = Math.min(maxNumberOfSteps, 1 + 5 - x % 5 + minimumOperationsToMakeEqual(x / 5 + 1, y));
+
+        return maxNumberOfSteps;
+    }
+
+    // Time: O(n)
+    // Space: O(n)
+    public int minimumOperationsToMakeEqualBfs(int x, int y) {
+        if (x == y) {
+            return 0;
+        }
+        if (x < y) {
+            return y - x;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(x);
+        Set<Integer> visited = new HashSet<>();
+        int level = 1;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int node = queue.poll();
+                List<Integer> adjNodes = getAdjNodes(node);
+                for (int adjNode : adjNodes) {
+                    if (adjNode == y) return level;
+                    if (!visited.contains(adjNode)) {
+                        queue.add(adjNode);
+                        visited.add(adjNode);
+                    }
+                }
+            }
+            level++;
+        }
+        return level;
+    }
+
+    private List<Integer> getAdjNodes(int node) {
+        List<Integer> adjNodes = new ArrayList<>();
+        adjNodes.add(node - 1);
+        adjNodes.add(node + 1);
+        if (node % 11 == 0) adjNodes.add(node / 11);
+        if (node % 5 == 0) adjNodes.add(node / 5);
+        return adjNodes;
+    }
+
+}
---
+++ src/test/java/leetcode/minimumNumberOfOperationsToMakeXAndYEqual/Solution10033Test.java
@@ -0,0 +1,37 @@
+package leetcode.minimumNumberOfOperationsToMakeXAndYEqual;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Solution10033Test {
+
+    static Stream<Arguments> casesProvider() {
+        return Stream.of(
+                Arguments.of(26, 1, 3),
+                Arguments.of(54, 2, 4),
+                Arguments.of(25, 30, 5),
+                Arguments.of(4, 2, 2),
+                Arguments.of(8, 2, 3),
+                Arguments.of(13, 1, 3),
+                Arguments.of(13, 3, 3)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("casesProvider")
+    void minimumOperationsToMakeEqual(int x, int y, int expected) {
+        assertEquals(expected, new Solution10033().minimumOperationsToMakeEqual(x, y));
+    }
+
+    @ParameterizedTest
+    @MethodSource("casesProvider")
+    void minimumOperationsToMakeEqualBfs(int x, int y, int expected) {
+        assertEquals(expected, new Solution10033().minimumOperationsToMakeEqualBfs(x, y));
+    }
+
+}
