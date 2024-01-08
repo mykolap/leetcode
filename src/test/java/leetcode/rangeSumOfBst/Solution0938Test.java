d5ed4751b2edc7d98a0ea658eaa13f8917ace63d
Mykola Pavluchynskyi <mpa@encode.dk>
2024-01-08 19:14:00 +0100

    938. Range Sum of BST

---
+++ src/test/java/leetcode/rangeSumOfBst/Solution0938Test.java
@@ -0,0 +1,38 @@
+package leetcode.rangeSumOfBst;
+
+import leetcode.common.TreeNode;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Solution0938Test {
+
+    static Stream<Arguments> casesProvider() {
+        return Stream.of(
+                Arguments.of(TreeNode.fromVarargArray(10, 5, 15, 3, 7, null, 18),
+                        7, 15,
+                        32),
+                Arguments.of(TreeNode.fromVarargArray(10, 5, 15, 3, 7, 13, 18, 1, null, 6),
+                        6, 10,
+                        23)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("casesProvider")
+    void rangeSumBstBfs(TreeNode root, int low, int high, int expected) {
+        int actual = new Solution0938().rangeSumBstBfs(root, low, high);
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @MethodSource("casesProvider")
+    void rangeSumBstDfs(TreeNode root, int low, int high, int expected) {
+        int actual = new Solution0938().rangeSumBstDfs(root, low, high);
+        assertEquals(expected, actual);
+    }
+}
