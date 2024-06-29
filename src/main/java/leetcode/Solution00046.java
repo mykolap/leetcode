40e294b909a2152174bf5d3c26639d65e03eae72
Mykola Pavluchynskyi <mpa@encodify.com>
2024-06-29 11:56:05 +0200

    46. Permutations

--- src/main/java/leetcode/Solution00046.java
+++ src/main/java/leetcode/Solution00046.java
@@ -1,50 +1,38 @@
 package leetcode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
  * 46. Permutations
  * https://leetcode.com/problems/permutations/
- * <p>
  * Medium
- * <p>
- * Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any
- * order.
- * <p>
- * Example 1:
- * <p>
- * Input: nums = [1,2,3] Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]] Example 2:
- * <p>
- * Input: nums = [0,1] Output: [[0,1],[1,0]] Example 3:
- * <p>
- * Input: nums = [1] Output: [[1]]
- * <p>
- * Constraints:
- * <p>
- * 1 <= nums.length <= 6 -10 <= nums[i] <= 10 All the integers of nums are unique.
  */
 public class Solution00046 {
 
-    // Time: O(n * n!)
-    // Space: O(n * n!)
     public List<List<Integer>> permute(int[] nums) {
+        List<Integer> numsList = Arrays.stream(nums).boxed().toList();
+        return permute(numsList);
+    }
+
+    public List<List<Integer>> permute(List<Integer> numsList) {
+        if (numsList.isEmpty()) {
+            return List.of(List.of());
+        }
+
+        final int first = numsList.getFirst();
+        List<List<Integer>> permuteNext = permute(numsList.subList(1, numsList.size()));
         List<List<Integer>> result = new ArrayList<>();
-        permute(nums, 0, new ArrayList<>(), result);
+        for (List<Integer> perm : permuteNext) {
+            for (int i = 0; i <= perm.size(); i++) {
+                List<Integer> newPerm = new ArrayList<>(perm);
+                newPerm.add(i, first);
+                result.add(newPerm);
+            }
+        }
         return result;
     }
 
-    private void permute(int[] nums, int index, List<Integer> current, List<List<Integer>> result) {
-        if (index == nums.length) {
-            result.add(new ArrayList<>(current));
-            return;
-        }
-
-        for (int i = 0; i <= current.size(); i++) {
-            current.add(i, nums[index]);
-            permute(nums, index + 1, current, result);
-            current.remove(i);
-        }
-    }
 
 }
--- src/test/java/leetcode/Solution00046Test.java
+++ src/test/java/leetcode/Solution00046Test.java
@@ -1,5 +1,6 @@
 package leetcode;
 
+import leetcode.common.ArrayUtils;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -11,42 +12,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class Solution00046Test {
 
-    static Stream<Arguments> casesProvider() {
-        return Stream.of(
-                Arguments.of(
-                        new int[]{1, 2, 3},
-                        List.of(
-                                List.of(3, 2, 1),
-                                List.of(2, 3, 1),
-                                List.of(2, 1, 3),
-                                List.of(3, 1, 2),
-                                List.of(1, 3, 2),
-                                List.of(1, 2, 3)
-                        )
-                ),
-                Arguments.of(
-                        new int[]{0, 1},
-                        List.of(
-                                List.of(1, 0),
-                                List.of(0, 1)
-                        )
-                ),
-                Arguments.of(
-                        new int[]{1},
-                        List.of(
-                                List.of(1)
-                        )
-                )
-        );
-    }
-
     @ParameterizedTest
     @MethodSource("casesProvider")
-    void permute(int[] nums, List<List<Integer>> expected) {
-        var solution = new Solution00046();
-        var actual = solution.permute(nums);
-        assertThat(actual)
-                .containsExactlyElementsOf(expected);
+    void permute(String numsStr, String expected) {
+        int[] nums = ArrayUtils.stringToIntArray(numsStr);
+        List<List<Integer>> actual = new Solution00046().permute(nums);
+        assertThat(ArrayUtils.listOfListsToString(actual))
+                .isEqualTo(expected);
+    }
+
+    static Stream<Arguments> casesProvider() {
+        return Stream.of(
+                Arguments.of("[1,2,3]", "[[1,2,3],[2,1,3],[2,3,1],[1,3,2],[3,1,2],[3,2,1]]"),
+                Arguments.of("[0,1]", "[[0,1],[1,0]]"),
+                Arguments.of("[1]", "[[1]]")
+        );
     }
 
 }
