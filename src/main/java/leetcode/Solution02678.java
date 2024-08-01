334fee3df2a4ba1c80a10272c14a55d7d6a98101
Mykola Pavluchynskyi <mpa@encodify.com>
2024-08-01 22:00:06 +0200

    2678. Number of Senior Citizens

--- src/main/java/leetcode/Solution02678.java
+++ src/main/java/leetcode/Solution02678.java
@@ -11,17 +11,10 @@ public class Solution02678 {
     public int countSeniors(String[] details) {
         int count = 0;
         for (String detail : details) {
-            int firstDigit = detail.charAt(11) - '0';
-            if (firstDigit < 6) {
-                continue;
+            if (detail.charAt(11) > '6' ||
+                (detail.charAt(11) == '6' && detail.charAt(12) != '0')) {
+                count++;
             }
-            if (firstDigit == 6) {
-                int secondDigit = detail.charAt(12) - '0';
-                if (secondDigit == 0) {
-                    continue;
-                }
-            }
-            count++;
         }
         return count;
     }
